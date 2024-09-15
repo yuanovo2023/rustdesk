970941e7cce04078ffd50c09c7cef2af58813242
yuanovo2023 <133623267+yuanovo2023@users.noreply.github.com>
2024-09-15 21:08:40 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["47.76.110.180"];
-pub const PUBLIC_RS_PUB_KEY: &str = "JW1wRJry5kiF9vgGQt2CmTOdgj8Acle2LnDQJAIfOrk=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["beedesk.beesoft.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "VSPdimnD2rbIsIshJqMOya3kghwY8CHXyP+nhrWk0kI=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
